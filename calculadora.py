ae51133a0c82312c08f7f34edfbe59615a107017
JorgeRuizParra <zx20student122@colegiovalledelmiro.es>
2023-09-26 13:42:39 +0200

    dividir

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,12 @@ var2 = 2
 
 function sumar() 
     print(var1+var2)
+
+function resta()
+    print(var1-var2)
+
+function multiplicar()
+    print(var1*var2)
+
+function dividir()
+    print(var1/var2)
